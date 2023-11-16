6a26e6db0cda6dd197fb8946433d38353d9a126e
S <teniacs@outlook.com>
2023-11-17 07:09:59 +0800

    Update memfd.rs

--- src/sys/memfd.rs
+++ src/sys/memfd.rs
@@ -50,7 +50,7 @@ pub fn memfd_create(name: &CStr, flags: MemFdCreateFlag) -> Result<OwnedFd> {
                 any(
                     target_os = "freebsd",
                     // If the OS is Linux, gnu and musl expose a memfd_create symbol but not uclibc
-                    target_env = "gnu",
+                    // target_env = "gnu",
                     target_env = "musl",
                 )))]
             {
